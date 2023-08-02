f26f70d74192e4e64c36660d7a19680875f08a1e
EricLiu27 <eric.liu82794@gmail.com>
2023-08-02 00:36:01 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>* Learn React</ul>
+      <ul>* Climb Mt. Everest</ul>
+      <ul>* Run a marathon</ul>
+      <ul>* Feed the dogs</ul>
+    </div>
+  );
+}
+
+export default App;
